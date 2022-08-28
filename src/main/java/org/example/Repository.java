2725a45607ed1a4196251254332b1480c232663f
Илья Черенков <cherenkov_ilya@tut.by>
2022-08-28 16:31:20 +0300

    tested with assertThrows

---
+++ src/main/java/org/example/Repository.java
@@ -0,0 +1,52 @@
+package org.example;
+
+public class Repository {
+
+
+
+    Product[] products = new Product[0];
+
+    public void add(Product product) {
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+    }
+
+
+    public void removeById(int removeId) {
+        if (findById(removeId) == null) {
+            throw new NotFoundException("Element with id: " + removeId + " not found");
+        }
+        Product[] tmp = new Product[products.length - 1];
+        int index = 0;
+        for (Product product : products) {
+            if (product.getId() != removeId) {
+                tmp[index] = product;
+                index++;
+            }
+        }
+        products = tmp;
+    }
+
+    public Product findById(int id) {
+        Product result = null;
+        for (Product product : products) {
+            if (product.getId() == id) {
+                result = product;
+                break;
+            }
+        }
+        return result;
+    }
+
+
+    public Product[] findAll() {
+        return products;
+    }
+
+
+}
+
---
+++ src/test/java/org/example/BookTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+
+    Book book = new Book(1, "Fire", 100, "Russell");
+
+
+    @Test
+    public void setAuthor() {
+        book.setAuthor("Black");
+
+        String expected = "Black";
+        String actual = book.getAuthor();
+        Assertions.assertEquals(expected, actual);
+    }
+}
